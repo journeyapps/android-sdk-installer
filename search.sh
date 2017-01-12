be9b7eb8f209452b89ba8037643e32ecbfcbd3ff
rasharab <rashin@buddybuild.com>
2017-01-12 13:30:46 -0800

    Add license information for mac

--- search.sh
+++ search.sh
@@ -1,9 +1,8 @@
-android list sdk -u -s -a |\
+  android list sdk -u -s -a |\
     # Filter obsoleted packages
     grep '\s\+\d\+-'      |\
-    sed '/\(Obsolete\)/d' |\
     sed '/\(Source\)/d' |\
     sed '/\(Documentation\)/d' |\
-    sed '/\(Intel\)/d' |\
-    sed 's/^[ ]*\([0-9]*\)-.*/\1/' |\
-    paste -d, -s -
+    sed '/\(MIPS\)/d' |\
+    sed '/\(Android Wear\)/d' |\
+    sed '/\(Android TV\)/d'
