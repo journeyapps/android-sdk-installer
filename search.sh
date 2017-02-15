ef4546f3fe85059ada4c1c54a5132dffde4ba6de
buddybuild <help@buddybuild.com>
2017-02-15 14:45:29 -0800

    Try again

--- search.sh
+++ search.sh
@@ -4,5 +4,8 @@
     sed '/\(Source\)/d' |\
     sed '/\(Documentation\)/d' |\
     sed '/\(MIPS\)/d' |\
+    sed '/\(Obsolete\)/d' |\
+    sed '/\(Intel\)/d' |\
     sed '/\(Android Wear\)/d' |\
-    sed '/\(Android TV\)/d'
+    sed '/\(Android TV\)/d' |\
+    sed '/\(ARM 64\)/d'
