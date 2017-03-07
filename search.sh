76d2b9b29f98d7ec8dbf1d58eceecd9f1b4f92d9
buddybuild <help@buddybuild.com>
2017-03-06 21:26:51 -0800

    Move to sdkmanager to install constraints etc.

--- search.sh
+++ search.sh
@@ -1,11 +1,16 @@
-  android list sdk -u -s -a |\
+ /Users/rasharab/.android-sdk-installer/android-sdk-macosx/tools/bin/sdkmanager --update
+ /Users/rasharab/.android-sdk-installer/android-sdk-macosx/tools/bin/sdkmanager --list --verbose |\
     # Filter obsoleted packages
-    grep '\s\+\d\+-'      |\
-    sed '/\(Source\)/d' |\
-    sed '/\(Documentation\)/d' |\
-    sed '/\(MIPS\)/d' |\
-    sed '/\(Obsolete\)/d' |\
-    sed '/\(Intel\)/d' |\
-    sed '/\(Android Wear\)/d' |\
-    sed '/\(Android TV\)/d' |\
-    sed '/\(ARM 64\)/d'
+    egrep '^\S+'      |\
+    sed '/\(---\)/d'   |\
+    sed '/\(intel\)/d'   |\
+    sed '/\(Info\)/d' |\
+    sed '/\(Installed\)/d' |\
+    sed '/\(Available\)/d' |\
+    sed '/\(sources\)/d' |\
+    sed '/\(docs\)/d' |\
+    sed '/\(mips\)/d' |\
+    sed '/\(android-wear\)/d' |\
+    sed '/\(x86\)/d' |\
+    sed '/\(arm64\)/d' |\
+    sed '/\(done\)/d' > test.txt
