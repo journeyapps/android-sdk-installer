fab136f2e4d096c948349a1ca4b51467f054796f
buddybuild <help@buddybuild.com>
2017-03-07 19:05:15 -0800

    ADd file

--- search.sh
+++ search.sh
@@ -2,16 +2,23 @@
  /Users/rasharab/.android-sdk-installer/android-sdk-macosx/tools/bin/sdkmanager --list --verbose |\
     # Filter obsoleted packages
     egrep '^\S+'      |\
-    sed '/\(---\)/d'   |\
-    sed '/\(intel\)/d'   |\
-    sed '/\(Info\)/d' |\
-    sed '/\(Installed\)/d' |\
-    sed '/\(Available\)/d' |\
-    sed '/\(sources\)/d' |\
-    sed '/\(docs\)/d' |\
-    sed '/\(mips\)/d' |\
-    sed '/\(android-wear\)/d' |\
-    sed '/\(x86\)/d' |\
-    sed '/\(arm64\)/d' |\
-    sed '/\(^tools\)/d' |\
-    sed '/\(done\)/d' > test.txt
+    sed -E '/(---)/d'  |\
+    sed -E '/(Info)/d' |\
+    sed -E '/(Installed)/d' |\
+    sed -E '/(Available)/d' |\
+    sed -E '/(sources)/d' |\
+    sed -E '/(docs)/d' |\
+    sed -E '/(mips)/d' |\
+    sed -E '/(intel)/d' |\
+    sed -E '/(android-tv)/d' |\
+    sed -E '/(android-wear)/d' |\
+    sed -E '/(usb)/d' |\
+    sed -E '/(x86)/d' |\
+    sed -E '/(arm64)/d' |\
+    sed -E '/(platforms;android-(7|8|9))/d' |\
+    sed -E '/(add-ons;addon-google_apis-google-(15|16|17|18|19))/d' |\
+    sed -E '/(system-images;android-(10|14|15|16|17|18|19|21|22);)/d' |\
+    # avoid installing emulator/tools due to forcing to 25.2.5
+    sed -E '/(^emulator)/d' |\
+    sed -E '/(^tools)/d' |\
+    sed '/\(done\)/d' > sdklist.txt
